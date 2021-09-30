720aa93f65adc6d8511ee748316f1a55071e24dd
lucifer1112k <jamiulislamjami@hotmail.com>
2021-09-30 19:32:55 +0600

    added-portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,10 +64,19 @@ const educationClick = () => {
   });
 };
 educationClick();
-/*==================== SERVICES MODAL ====================*/
-
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
